619e56df278950027077055cbc6a9d6c5127c2a5
Prateek_Gupta <prateekgupta4341@gmail.com>
2022-09-13 20:59:12 +0530

    variabls

---
+++ variable.js
@@ -0,0 +1,14 @@
+let b = 23
+console.log(b)
+c = "prt"
+console.log(c)  
+// let 6asd = 69
+// console.log(6asd)
+
+const d = 'anil'
+// d = hiil       [Error: hiil is not defined]
+console.log(d)
+
+var e = 43
+e= 34
+console.log(e)

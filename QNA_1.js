0934698f4f16324f9c6a8bf6e853b0d9b4781134
Prateek_Gupta <prateekgupta4341@gmail.com>
2022-09-15 20:45:50 +0530

    basic problems

---
+++ QNA_1.js
@@ -0,0 +1,33 @@
+ let a = "amit"
+ let b = 5
+ console.log(a+b)
+
+ console.log(typeof(a+b))
+
+ const brah = {
+
+    mnp : "we",
+    soprt : 34,
+    scgf :  true
+ }
+
+//  brah = 45  Error: Assignment to constant variable.
+
+
+brah['frnf'] = "chk_ha"  
+// add a new key to a object
+
+
+brah["mnp"] = "chnged using this"
+// chng a key 
+
+console.log(brah)
+
+const dictnry = {
+    she : "female",
+    he : "male",
+    femalegurls : "she"
+
+}
+
+console.log(dictnry.he)

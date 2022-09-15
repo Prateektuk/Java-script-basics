cc32106b1a88b8cb46d440d44ed4c2bf7e0b25b1
Prateek_Gupta <prateekgupta4341@gmail.com>
2022-09-15 19:45:06 +0530

    about data type

---
+++ datatyp_objects.js
@@ -0,0 +1,23 @@
+// nn bb ss u pritive dat_types
+
+a = null
+b = 234;
+c = true
+d = BigInt("420") + BigInt("10")
+e = "cheep"
+f = Symbol("free")
+g = undefined
+
+console.log(a,b,c,d,e,f,g)
+console.log(typeof(d))
+// objects  "nonpritive"
+
+const inmt = {
+    nme : "prateek",
+    age : 20,
+    happy : false
+}
+
+console.log(inmt["nme"])
+
+

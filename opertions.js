fa6633db0d6ce44a7695f5970226ceea7a059948
Prateek_Gupta <prateekgupta4341@gmail.com>
2022-09-20 20:08:59 +0530

    operations

---
+++ opertions.js
@@ -0,0 +1,16 @@
+let a = prompt("Name?")
+
+a = Number.parseInt(a)
+
+if(a>0){
+    alert("invalide X!");
+}
+
+else if(a<18){
+    alert("nt allowed")
+}
+else {
+    alert("correct");
+}
+
+console.log(typeof(a))

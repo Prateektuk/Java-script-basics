f3d33d73dab4ce1557bbbfd6b5dc9b7fdde85ef1
Prateek_Gupta <prateekgupta4341@gmail.com>
2022-10-04 03:43:46 +0530

    Ques and ans for practce

---
+++ QNA_2.js
@@ -0,0 +1,41 @@
+// let age = prompt("enter age?")
+// age = Number.parseInt(age)
+// if(age>10 && age<20){
+//      alert("age between 10 and 20!")
+// }
+// else{
+//     alert("age not between 10 and 20!")
+// }
+
+
+// let age = prompt("age batao?")
+// switch(age){
+//     case '12' :
+//         console.log("12")
+//         break
+//     case '13' :
+//         console.log("13")
+//         break
+//     case '14 ':
+//         console.log("14")
+//         breaK
+//     case '15' :
+//         console.log("15")
+//         break 
+//     default :
+//         console.log("not special")   
+// }
+
+// let no = prompt("num:")
+// no = Number.parseInt(no)
+
+// if((no%2 == 0) && (no%3 == 0)){
+//     console.log("divi by 2&3")}
+// else{console.log("not divi by 2&3")}
+
+
+// TERNARY OOPERATORS
+let age =prompt("enter age?")
+age = Number.parseInt(age) 
+let a = age >= 18 ? "u cn driv" : "u cnnot driv"
+console.log(a)

edf7bec6b8516f3795932abab5476604907bdb3d
Prateek_Gupta <prateekgupta4341@gmail.com>
2022-10-04 03:46:55 +0530

    for loop basic

---
+++ for_loops.js
@@ -0,0 +1,25 @@
+// let sum = 0
+// let n = prompt("enter the no:")
+// n = Number.parseInt(n)
+
+
+// for (let i = 1; i<n; i++){ sum += i}
+
+// console.log(sum)
+
+let stu = {
+  hary : 34,
+  shub : 23,
+  shiv : 45 ,
+  riti : 12 
+}
+// for in loop
+for (let a in stu){
+    console.log("marks of " + a + " are " +stu[a])
+}
+
+// for of loop
+
+for (let b of "jass"){
+    console.log(b)
+}

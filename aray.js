967206548972ecb8d0db8391bd46b24334fd8ff7
Prateektuk <prateekgupta4341@gamil.com>
2022-12-14 17:00:01 +0530

    arrays oprsons

---
+++ aray.js
@@ -0,0 +1,20 @@
+let num = [1,2,3,34,4]
+let b = num.toString();  
+
+// console.log(b)
+// console.log(typeof(b))
+
+let c = num.join('_')
+
+// console.log(c, typeof(c)) 
+
+// Pop()
+// console.log(num.pop(),num)
+
+// Push()
+
+// console.log(num.push(69)) retruns index of pushed  element 
+
+let dum = [1,2,3,4,5,6,7,8,9]
+delete dum[0]
+console.log(dum)

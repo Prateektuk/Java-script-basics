7dd4d609d0f0479b8c76f8be7f7ebaa43334d1de
Prateek_Gupta <91743041+Prateektuk@users.noreply.github.com>
2022-12-14 17:28:25 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,15 @@
+// let name = "hary";
+// console.log(name)
+// consol e.log(name.length)
+// console.log(name[2]);
+
+// template literals
+
+let b1 = "qwe"
+let b2 = "rty"
+let snetns =  ` ${b1} is related to ${b2}`
+// this is string interpulation
+console.log(snetns)
+
+// escape sequense Character
+console.log('tru\'hai')
---
+++ string_2.js
@@ -0,0 +1,18 @@
+let a = "pra teek"
+console.log(a.toUpperCase())
+ 
+
+console.log(a.slice(2,4))
+console.log(a.slice(2))
+
+console.log(a.replace("teek","chi"))
+
+console.log(a)
+
+
+let frd = "    chaman   ";
+
+console.log(frd.trim())
+console.log(frd + a)
+
+
